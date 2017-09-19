a82ea5b29cd706daaf2e4453f4f8fe991292c270
bill <bill@trillioninnovations.com>
2017-09-19 10:28:17 +0800

    add webhook, verify_token

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const verify_token = "bill_test_messenger_bot"
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -8,3 +9,14 @@ app.get('/', function (req, res) {
 app.listen(3256, function () {
   console.log('Example app listening on port 3256!')
 })
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === verify_token) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
